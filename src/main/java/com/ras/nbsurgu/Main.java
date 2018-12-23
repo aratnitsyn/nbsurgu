1816fa1125278985b4e87ea23d787ddc8875fb12
81x08 <nixon133@mail.ru>
2018-12-23 22:15:41 +0500

    Инициализация парсера

--- src/main/java/com/ras/nbsurgu/Main.java
+++ src/main/java/com/ras/nbsurgu/Main.java
@@ -1,11 +1,28 @@
 package com.ras.nbsurgu;
 
 import com.ras.nbsurgu.telegram.Telegram;
+import com.ras.nbsurgu.telegram.utils.XMLParser;
+
+import java.io.IOException;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 public class Main {
 
     public static void main(String[] args) {
+        init();
+
         new Telegram().start();
+    }
+
+    private static void init() {
+        try {
+            new XMLParser().init();
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+        }
     }
 
 }
